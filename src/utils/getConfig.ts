96163396387eab61b545e10abfdf3bbb2fd62355
Jake Adler <jamtheproducer@gmail.com>
2020-06-25 13:30:09 -0700

    getConfig hotfix

--- src/utils/getConfig.ts
+++ src/utils/getConfig.ts
@@ -7,14 +7,17 @@ import parseArgs from "yargs-parser";
 import chalk from "chalk";
 
 function getConfig() {
-  let config, verbose;
+  let config: string;
+  let verbose: boolean;
+  let paths: string[];
 
   const argv = parseArgs(process.argv);
 
   if (argv["c"] || argv["config"]) config = argv["c"] || argv["config"];
   if (argv["v"] || argv["verbose"]) verbose = argv["v"] || argv["verbose"];
-  const paths: string[] = find.fileSync(/config.yaml/g, `${__dirname}/`);
+
   if (!config) {
+    paths = find.files(/config.yaml/g, `${__dirname}/`);
     if (paths.length === 0) {
       log.warn(
         `${chalk.bold.yellow(
@@ -30,16 +33,15 @@ function getConfig() {
   }
 
   let configFile: string;
+  let userConfig;
 
   if (config) {
     configFile = config;
-  } else {
+  } else if (paths.length === 1) {
     configFile = paths[0];
   }
 
   if (verbose) log(`Config file detected at ${configFile}`);
-
-  let userConfig;
 
   if (configFile) {
     const configRaw = fs.readFileSync(configFile, "utf8");
