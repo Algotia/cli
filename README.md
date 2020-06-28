2a36b26120893d577bde666876858752c16019c0
Jake Adler <jamtheproducer@gmail.com>
2020-06-28 00:57:09 -0700

    added correct workflow, and testing breaking build

---
+++ README.md
@@ -0,0 +1,1 @@
+[![CodeFactor](https://www.codefactor.io/repository/github/algotia/cli/badge)](https://www.codefactor.io/repository/github/algotia/cli)
--- dist/cli.js
+++ dist/cli.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,7 +7,8 @@ const core_1 = require("@algotia/core");
 const index_1 = require("./utils/index");
 const commander_1 = __importDefault(require("commander"));
 const createCli_1 = __importDefault(require("./lib/createCli"));
-(() => __awaiter(void 0, void 0, void 0, function* () {
+(sync());
+{
     try {
         // Register the initial options
         commander_1.default
@@ -28,10 +20,11 @@ const createCli_1 = __importDefault(require("./lib/createCli"));
         const bootOptions = {
             verbose: commander_1.default.verbose
         };
-        const bootData = yield core_1.boot(config, bootOptions);
+        const bootData = await core_1.boot(config, bootOptions);
         createCli_1.default(bootData);
     }
     catch (err) {
         console.log(err);
     }
-}))();
+}
+();
